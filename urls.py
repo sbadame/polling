5b72611501218afb41983c70fe5032983b838449
Sandro <sandro@sbcoded.com>
2012-02-01 20:58:20 -0600

    Fixed one url link

--- urls.py
+++ urls.py
@@ -2,17 +2,15 @@ from django.conf.urls.defaults import patterns, include, url
 from django.views.generic import DetailView, ListView
 from polls.models import Poll
 
+poll_view_function = DetailView.as_view( model=Poll, template_name='detail.html')
+
 urlpatterns = patterns('',
     url(r'^$',
         ListView.as_view(
             queryset=Poll.objects.order_by("-date_created"),
             context_object_name='latest_poll_list',
             template_name='index.html')),
-    url(r'^(?P<pk>\d+)/$',
-        DetailView.as_view(
-            model=Poll,
-            template_name='detail.html'),
-        name = 'poll_view'),
+    url(r'^(?P<pk>\d+)/$', poll_view_function, name = 'poll_view'),
     url(r'^(?P<pk>\d+)/results/$',
         DetailView.as_view(
             model=Poll,
