8f2fd624b64528d231c5ff18062d2459cb55459f
Perro <cmperro@gmail.com>
2012-02-08 20:36:12 -0600

    Fancy Bootstrap stuff starting to come along. Fancy smancy

--- urls.py
+++ urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, TemplateView
 from polls.models import Poll
 
 poll_view_function = DetailView.as_view( model=Poll, template_name='detail.html')
+index_view_function = ListView.as_view(
+        queryset=Poll.objects.order_by("-date_created")[:10],
+            context_object_name='latest_poll_list',
+            template_name='index.html')
+
 
 urlpatterns = patterns('',
-    url(r'^$',
-        ListView.as_view(
-            queryset=Poll.objects.order_by("-date_created"),
-            context_object_name='latest_poll_list',
-            template_name='index.html')),
+    url(r'^$',index_view_function),
     url(r'^(?P<pk>\d+)/$', poll_view_function, name = 'poll_view'),
     url(r'^(?P<pk>\d+)/results/$',
         DetailView.as_view(
