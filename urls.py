57d3cd3933da5806163fbecd0d842e193dbf7493
Sandro <sandro@sbcoded.com>
2012-02-11 10:40:16 -0600

    - Added a custom handler for search in urls.py - Moved search.html up a directory - killed poll_text.txt because it's useless and everything seems fine without it.

--- urls.py
+++ urls.py
@@ -2,6 +2,8 @@ from django.conf.urls.defaults import patterns, include, url
 from django.views.generic import DetailView, ListView, TemplateView
 from polls.models import Poll
 import polls
+import haystack
+import haystack.views
 
 about_view = TemplateView.as_view(template_name='about.html')
 
@@ -10,6 +12,14 @@ urlpatterns = patterns('',
     url(r'^(?P<poll_id>\d+)/$', 'polls.views.view', name = 'poll_view'),
     url(r'^create$', 'polls.views.create'),
     url(r'^about$', about_view),
-    url(r'^search/', include('haystack.urls')),
     url(r'^(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+
+    # Search page(s)
+    url(r'^search/',
+        haystack.views.search_view_factory(
+            view_class=haystack.views.SearchView,
+            template='search.html',
+            form_class=haystack.forms.SearchForm
+        ),
+        name='search'),
 )
