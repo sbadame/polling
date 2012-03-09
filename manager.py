4571f45b2020d73014c99d68daa33955204e6eb2
Sandro <sandro@sbcoded.com>
2012-03-09 00:16:48 -0600

    Very weird things happening with manage.py... ghostly output is not capturable...

--- manager.py
+++ manager.py
@@ -22,14 +22,14 @@ def memcached():
 def solr():
     out, err = run("java -jar start.jar", waitFor="Started", cwd="solr/example")
 
-def django():
-    pass
+def runserver():
+    run("python manage.py runserver 0.0.0.0:2869", waitFor="server is running")
 
 def start_app():
     mysql()
     memcached()
     solr()
-    django()
+    runserver()
 
 def update():
     out,err = run('git status')
@@ -58,7 +58,7 @@ def run(cmd, waitForExit=True, waitFor=None, waitSeconds=1, **kwargs):
     p = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs)
     print(" has pid: %d" % p.pid)
 
-    if waitForExit:
+    if not waitFor and waitForExit:
         out,err = p.communicate()
         sys.stdout.write(out)
         sys.stderr.write(err)
@@ -91,27 +91,29 @@ def run(cmd, waitForExit=True, waitFor=None, waitSeconds=1, **kwargs):
 
         def keeploop():
             if waitFor:
-                return True
+                return time.clock() - start_time <= 5
             else:
                 return time.clock() - start_time <= waitSeconds
 
-        while keeploop():
-            try: line = out_queue.get_nowait()
+        while p.poll() is None and keeploop():
+            try: outline = out_queue.get_nowait()
             except Empty:
                 pass
             else:
-                sys.stdout.write(line)
-                if waitFor and waitFor in line:
+                sys.stdout.write(outline)
+                if waitFor and waitFor in outline:
                     break
 
-            try: line = err_queue.get_nowait()
+            try: errline = err_queue.get_nowait()
             except Empty:
                 pass
             else:
-                sys.stderr.write(line)
-                if waitFor and waitFor in line:
+                sys.stderr.write(errline)
+                if waitFor and waitFor in errline:
                     break
 
+        sys.stdout.flush()
+        sys.stderr.flush()
         if not p.poll() is None: #The process should not have terminated...
             error("command: %s with pid: %d terminated with exit code: %d" % (cmd, p.pid, p.returncode))
 
@@ -122,10 +124,9 @@ def run(cmd, waitForExit=True, waitFor=None, waitSeconds=1, **kwargs):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='Does all of the ugly shell crap')
-    parser.add_argument('program', choices=["update", "mysql", "memcached", "solr"])
+    parser.add_argument('program', choices=["runserver", "update", "mysql", "memcached", "solr"])
     parser.add_argument('args', nargs='*')
     args = vars(parser.parse_args(sys.argv[1:]))
     if args["program"] in globals():
         globals()[args["program"]](*args['args'])
-
 
